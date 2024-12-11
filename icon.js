99181e231152efd28be0fff4959087d73d342140
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2024-12-11 16:58:11 +0530

    Historical Data Baki 11-12-24 4:58

---
+++ icon.js
@@ -0,0 +1,29 @@
+ Temperature °C
+ Wind ༄
+ WindDirection Degress
+ Altitude h
+ Humdity
+ CO₂
+ waterLevel
+ WaterTemperature 
+ WaterFlowRate m³/s 
+ GrounTemperature
+ GroundComposition    
+ DamPressure
+ Phvalue
+ IceThickness
+ RainFallIntensity
+ SnowFallRate
+ IceMeltRate
+ LandSlideRisk    
+ AvalancheRisk
+ EarthquackRisk
+ AlertType  
+<i class="fa-regular fa-clipboard"></i> ObservationNotes
+<i class="fa-solid fa-video"></i> RecordedAT  
+
+Lake Size
+Ground Pressure
+Soil Temperature
+Soil Moisture
+Precipitation
